52235bd3a871b06d4a5dd4f79125c8373fda744d
aku_jr <juniorntabe@gmail.com>
2021-09-28 07:50:11 +0100

    Update Exercise7.java

--- Exercise7.java
+++ Exercise7.java
@@ -1,7 +1,7 @@
 //Write a program by creating an 'Employee' class having the following methods and print the final salary.
 //1 - 'getInfo()' which takes the salary, number of hours of work per day of employee as parameter
 //2 - 'AddSal()' which adds $10 to salary of the employee if it is less than $500.
-//3 - 'AddWork()' which adds $5 to salary of employee if the number of hours of work per day is more than 6 hours.
+//3 - 'AddWork()' which adds $5 to salary of an employee if the number of hours of work per day is more than 6 hours.
 
 
 import java.util.Scanner;
